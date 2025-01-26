29471420346dae382d009ac8d3bea47e826c8be6
Razin-maker <razinahmed60@gmail.com>
2025-01-26 23:53:31 +0600

    Delete CLP-01/Task-04.py

--- CLP-01/Task-04.py
+++
@@ -1,7 +0,0 @@
-nums = [15, 26, 48, 50, 89, 65, 2, 75, 6]
-
-des_sorted_nums = sorted(set(nums), reverse=False)
-
-second_highest_num = des_sorted_nums[1]
-
-print("The second Highest number is: ", second_highest_num)
